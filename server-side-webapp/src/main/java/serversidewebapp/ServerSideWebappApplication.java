9bf1a5e1f46c5784e2c42bdefe03ed24b057fc2a
Gabriele Falace <g.falace@pinguin.ag>
2015-09-10 14:26:28 +0200

    initial commit

---
+++ server-side-webapp/src/main/java/serversidewebapp/ServerSideWebappApplication.java
@@ -0,0 +1,12 @@
+package serversidewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSideWebappApplication.class, args);
+    }
+}
